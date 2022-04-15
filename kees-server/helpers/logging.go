c89e09eea26da682b0f99da3adf7d02677364ed7
Nima H. Mood <nhmood@goosecode.com>
2022-04-15 19:23:16 -0400

    Add missing logging helper file (forgot on initial commit)

---
+++ kees-server/helpers/logging.go
@@ -0,0 +1,30 @@
+package helpers
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"kees-server/config"
+	"runtime"
+)
+
+var debug bool
+
+func ConfigureLogging(config config.LoggingConfig) {
+	debug = config.Debug
+	spew.Config.MaxDepth = config.SpewDepth
+}
+
+func Dump(data interface{}) {
+	spew.Dump(data)
+}
+
+func Debug(data interface{}) {
+	if debug {
+		_, file, no, ok := runtime.Caller(1)
+		if ok {
+			fmt.Printf("called from %s#%d\n", file, no)
+		}
+		Dump(data)
+		fmt.Printf("===================================\n\n\n")
+	}
+}
