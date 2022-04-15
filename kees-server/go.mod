33adfec291b33c37483295915f4adb5c4f5c7627
Nima H. Mood <nhmood@goosecode.com>
2022-04-15 19:20:57 -0400

    Add JWT helper/support

--- kees-server/go.mod
+++ kees-server/go.mod
@@ -4,8 +4,10 @@ go 1.15
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.5.0
 	github.com/mattn/go-sqlite3 v1.14.12
 	gopkg.in/yaml.v2 v2.4.0
 )
---
+++ kees-server/helpers/jwt.go
@@ -0,0 +1,65 @@
+package helpers
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+
+	"kees-server/config"
+)
+
+var jwtConfiguration config.JWTConfig
+
+func ConfigureJWT(config config.JWTConfig) {
+	jwtConfiguration = config
+}
+
+func GenerateJWT(data map[string]string) (string, int64, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	// Standard claims
+	// TODO - create proper claims struct
+	exp := time.Now().AddDate(0, 0, 1).Unix()
+	claims["exp"] = exp
+	claims["iss"] = jwtConfiguration.Issuer
+
+	// TODO: replace with function pointer to specific claim updater function
+	claims["kees"] = map[string]string{
+		"name":       data["name"],
+		"version":    data["version"],
+		"controller": data["controller"],
+	}
+
+	Debug(claims)
+
+	// Generate JWT with signing key from config
+	jwt, err := token.SignedString([]byte(jwtConfiguration.SigningKey))
+	if err != nil {
+		// TODO: don't love empty string vs nil, maybe change to str ptr
+		return "", 0, err
+	}
+
+	expiresIn := exp - time.Now().Unix()
+
+	return jwt, expiresIn, nil
+}
+
+func ValidateJWT(tokenString string) (jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(jwtConfiguration.SigningKey), nil
+	})
+
+	// TODO - handle individual token failures (expired, etc)
+	if err != nil {
+		Debug(err)
+		return nil, err
+	}
+
+	return claims, nil
+}
