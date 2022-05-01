9901aed8f8bef7815eea2dae3e23f352c8dbb8f1
Nima H. Mood <nhmood@goosecode.com>
2022-04-30 23:11:54 -0400

    Add scheme passing to baseURL and separate Authenticate function to own file

---
+++ kees-client/device/authenticate.go
@@ -0,0 +1,50 @@
+package device
+
+import (
+	"bytes"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Masterminds/log-go"
+
+	"kees-client/constants"
+	"kees-client/helpers"
+)
+
+func (c *Client) Authenticate() {
+	log.Info("Authenticating " + c.Device.Name)
+	jsonData, err := helpers.Format(c.Device)
+	if err != nil {
+		log.Warn("Failed to format Device info")
+		os.Exit(1)
+	}
+
+	request, err := http.NewRequest(
+		http.MethodPost,
+		c.getAuthURL("http"),
+		bytes.NewBuffer(jsonData),
+	)
+	request.Header.Set("User-Agent", "kees-client/"+constants.Version)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("X-Kees-MC-Token", c.Token)
+
+	httpClient := http.Client{Timeout: time.Duration(5 * time.Second)}
+	log.Info("Making request for: " + c.getAuthURL("http"))
+	resp, err := httpClient.Do(request)
+
+	if err != nil {
+		log.Warn("Failed to authenticate " + c.Device.Name)
+		log.Error(err)
+		os.Exit(1)
+	}
+
+	authResp := AuthResponse{}
+	helpers.Parse(resp, &authResp)
+	helpers.Debug(authResp)
+
+	c.Device = authResp.Device
+	c.Auth = authResp.JWT
+
+	log.Info("Authentication successful - DeviceID:" + c.Device.ID)
+}
--- kees-client/device/core.go
+++ kees-client/device/core.go
@@ -1,17 +1,12 @@
 package device
 
 import (
-	"bytes"
-	"net/http"
-	"os"
 	"sync"
-	"time"
 
 	"github.com/Masterminds/log-go"
 	"github.com/gorilla/websocket"
 
 	"kees-client/config"
-	"kees-client/constants"
 	"kees-client/helpers"
 	"kees-client/messages"
 )
@@ -72,51 +67,12 @@ func (c *Client) Run() {
 	//		 if expiration has passed
 	c.Authenticate()
 
-
-
 }
 
-func (c *Client) baseURL() string {
-	return "http://" + c.Server.Host + ":" + c.Server.Port
+func (c *Client) baseURL(scheme string) string {
+	return scheme + "://" + c.Server.Host + ":" + c.Server.Port
 }
 
-func (c *Client) getAuthURL() string {
-	return c.baseURL() + "/ws/v1/auth"
-}
-
-func (c *Client) Authenticate() {
-	log.Info("Authenticating " + c.Device.Name)
-	jsonData, err := helpers.Format(c.Device)
-	if err != nil {
-		log.Warn("Failed to format Device info")
-		os.Exit(1)
-	}
-
-	request, err := http.NewRequest(
-		http.MethodPost,
-		c.getAuthURL(),
-		bytes.NewBuffer(jsonData),
-	)
-	request.Header.Set("User-Agent", "kees-client/"+constants.Version)
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("X-Kees-MC-Token", c.Token)
-
-	httpClient := http.Client{Timeout: time.Duration(5 * time.Second)}
-	log.Info("Making request for: " + c.getAuthURL())
-	resp, err := httpClient.Do(request)
-
-	if err != nil {
-		log.Warn("Failed to authenticate " + c.Device.Name)
-		log.Error(err)
-		os.Exit(1)
-	}
-
-	authResp := AuthResponse{}
-	helpers.Parse(resp, &authResp)
-	helpers.Debug(authResp)
-
-	c.Device = authResp.Device
-	c.Auth = authResp.JWT
-
-	log.Info("Authentication successful - DeviceID:" + c.Device.ID)
+func (c *Client) getAuthURL(scheme string) string {
+	return c.baseURL(scheme) + "/ws/v1/auth"
 }
