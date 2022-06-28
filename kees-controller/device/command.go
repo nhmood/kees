653c5cf4bad656d685c614d2c9ab0f809a455d76
Nima H. Mood <nhmood@goosecode.com>
2022-06-27 23:50:16 -0400

    Remove explicit command handlers and move to config based command actions

--- kees-controller/device/command.go
+++ kees-controller/device/command.go
@@ -3,6 +3,7 @@ package device
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/Masterminds/log-go"
 
@@ -10,36 +11,30 @@ import (
 	"kees/controller/messages"
 )
 
-type CommandHandler func(*Controller) *messages.WebSocket
-
-var CommandHandlers = map[string]CommandHandler{
-	"play":         (*Controller).Play,
-	"stop":         (*Controller).Stop,
-	"rewind":       (*Controller).Rewind,
-	"fast_forward": (*Controller).FastForward,
-	"pause":        (*Controller).Pause,
-	"shuffle":      (*Controller).Shuffle,
-}
-
 func (c *Controller) Command(payload *messages.WebSocket) *messages.WebSocket {
 	helpers.Debug(payload)
-	command := payload.Data["command"].(string)
-	commandFunc := CommandHandlers[command]
-	if commandFunc == nil {
+	command := payload.Data["operation"].(string)
+	operation, ok := c.Device.Capabilities[command]
+	if !ok {
 		log.Error("Unknown command ", command)
 		err := formatMessage("error", "Unknown command", &payload.Data)
 		return &err
 	}
 
-	commandFunc(c)
+	runCommand(operation)
 
 	// TODO: acknowledge command status
 	return nil
 }
 
 func runCommand(command string) bool {
-	log.Info("Executing bin/irsend ", command)
-	cmd := exec.Command("bin/irsend", command)
+	log.Info("Executing ", command)
+
+	split := strings.Split(command, " ")
+	name := split[0]
+	args := split[1:]
+
+	cmd := exec.Command(name, args...)
 	output, err := cmd.Output()
 
 	if err != nil {
@@ -47,36 +42,6 @@ func runCommand(command string) bool {
 		return false
 	}
 
-	log.Info(fmt.Sprintf("irsend-> %s", output))
+	log.Info(fmt.Sprintf("%s -> %s", command, output))
 	return true
-}
-
-func (c *Controller) Play() *messages.WebSocket {
-	runCommand("play")
-	return nil
-}
-
-func (c *Controller) Stop() *messages.WebSocket {
-	runCommand("stop")
-	return nil
-}
-
-func (c *Controller) Rewind() *messages.WebSocket {
-	runCommand("rewind")
-	return nil
-}
-
-func (c *Controller) FastForward() *messages.WebSocket {
-	runCommand("fast_forward")
-	return nil
-}
-
-func (c *Controller) Pause() *messages.WebSocket {
-	runCommand("pause")
-	return nil
-}
-
-func (c *Controller) Shuffle() *messages.WebSocket {
-	runCommand("shuffle")
-	return nil
 }
